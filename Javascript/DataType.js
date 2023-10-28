eda43b8b7c30f6ed46ad011b5a25f56983ef7bbc
fiza-khatoon <91159093+fiza-khatoon@users.noreply.github.com>
2023-10-28 08:26:42 +0000

    DateTime

---
+++ Javascript/DataType.js
@@ -0,0 +1,17 @@
+console.log("Welcome to my House of JavaScript Secret")
+
+// .................................#######################...........................
+let a= "33"
+
+console.log(typeof a)
+console.log("   ")
+
+let check= Number(a)
+console.log(check);
+console.log(typeof(check)) 
+
+let check2=Boolean(a)
+console.log(check2);
+console.log(typeof check2);
+
+// /*You can take more datatype and cross check it with another datatype * 
