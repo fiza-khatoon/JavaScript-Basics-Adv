8b78d01d700a078409c1e01835224752fbc8671d
fiza-khatoon <91159093+fiza-khatoon@users.noreply.github.com>
2023-10-28 08:23:15 +0000

    AllData

---
+++ Javascript/AllData.js
@@ -0,0 +1,22 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>TYPES OF DATATYPES IN JS<<<<<<<<<<<<<
+
+// >>>>>>>>>>PRIMITIVE TYPES<<<<<<<<<<<<
+/*
+NUMBER
+
+BIGINT     let x = BigInt("123456789012345678901234567890");
+
+STRING
+
+BOOLEAN
+NULL
+UNDEFINED
+SYMBOL
+*/
+
+// >>>>>>>>>>>>>>NON PRIMITIVE TYPES<<<<<<<<<<<<<<
+/*
+ARRAY
+OBJECT
+FUNCTIONS
+*/
