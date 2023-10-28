9ca073f394690f9049216b86251e437cc89df04a
fiza-khatoon <91159093+fiza-khatoon@users.noreply.github.com>
2023-10-28 08:26:25 +0000

    Array3

---
+++ Javascript/Array3.js
@@ -0,0 +1,40 @@
+// >>>>>>>>>>>>>>concatenate arrays>>>>>>>>>>
+
+const arr=["My","name","is","fiza","sheikh."]
+const arr2=["I","am","from","India"]
+
+// >>>>>>>Push method to add two arrays>>>>>>>>>
+console.log("PUSH ARRAY RESULTS");
+// arr.push(arr2)
+// console.log(arr);
+// console.log(" ")
+
+
+// >>>>>>>>>>concatenate>>>>>>>>>>>>>>>
+console.log("CONCATENATION ARRAY");
+// let c=arr.concat(arr2)
+// console.log(c);
+// console.log(" ");
+
+// >>>>>>>>>>>>>SPREAD OPERATOR>>>>>>>>>>>>>
+console.log("SPREAD OPERATOR");
+// let d=[...arr,...arr2]
+// console.log(d);
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>More Methods to add arrays>>>>>>>>>>>>>>>>>>>
+
+let d=Array.from("Sheikh")
+
+console.log(d[3])
+// console.log(Array.from("Fiza")); /*give Fiza in Array format like----['f','i','z','a'] */
+
+// console.log(Array.from({name: "fiza"})); /*give empty array*/
+
+
+let a=10
+let b=45
+let c=36
+
+// console.log(Array.of(a,b,c))
+
