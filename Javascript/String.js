ebb1783d94cecc189eff4daa978e91e238a8c65f
fiza-khatoon <91159093+fiza-khatoon@users.noreply.github.com>
2023-10-28 08:29:05 +0000

    String

---
+++ Javascript/String.js
@@ -0,0 +1,15 @@
+// ***************************OPERATION**************************
+// String is primitive data type which stores characters or sets of characters
+
+console.log(1+1)
+console.log("\n")
+console.log(2+"2")
+console.log("\n");
+console.log("2"+2)
+console.log("\n");
+console.log(+"2"+3);
+console.log("\n")
+console.log(2 + +"2");
+
+
+console.log(+true)
