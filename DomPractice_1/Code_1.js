f2ecdd4467d6cdb089e752daee042792065d9be4
fiza-khatoon <91159093+fiza-khatoon@users.noreply.github.com>
2023-11-02 15:22:00 +0000

    DOM_1

---
+++ DomPractice_1/Code_1.js
@@ -0,0 +1,26 @@
+
+var a =document.querySelector("h1")
+// a.innerHTML="thankuhh every one";                   //changing html file
+// a.style.color= "black";                             //changine css file
+
+// changes 1 on h1
+
+// a.addEventListener("click",function(){              //Adding event listener i.e CLICK whenever you click on h1 text it will change the text
+//     a.innerHTML="Myself Fiza Sheikh"               //like this
+//     a.style.color="black"
+//     a.style.backgroundColor="white"
+// })
+
+// changes-2 in h2
+var b=document.querySelector("h2")
+
+b.addEventListener("click",function(){
+    b.innerHTML="Hey user i am Fiza khatoon.I am an engineer.Thankyou for visiting my profile"
+    b.style.color="black"
+    b.style.backgroundColor="yellow"
+
+})
+
+
+ 
+
