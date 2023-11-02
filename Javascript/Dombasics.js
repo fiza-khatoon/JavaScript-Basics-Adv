3ea8b33b40490a93d2bed74db277eacbda0c65c1
fiza-khatoon <91159093+fiza-khatoon@users.noreply.github.com>
2023-11-02 14:49:16 +0000

    Dombasic

---
+++ Javascript/Dombasics.js
@@ -0,0 +1,16 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.DATA OBJECT MODULATION>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// one of the most important topics in js is DOM:-The Document Object Model (DOM) connects web pages to scripts or programming languages by representing the structure of a document
+
+// Lets take an example suppose there is a h1 elements in html file and you want to fetch that h1 elemnts from html to js
+
+
+
+var a=document.querySelector("h1") //you can write in this way or like line number 12
+a.innerHTML="helllo everyone"
+a.style.color="red"
+
+document.querySelector("h1").innerHTML="hello guys"
+
+// >>>>>>>>>>lets make a folder that includes HTML CSS and JS and practice DOM there>>>>>>>>>>>>>
+
+
